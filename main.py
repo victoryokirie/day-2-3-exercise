9474d9e3b42e8371f1ba5018315de60671917b9e
Victory Okirie <78309631+victoryokirie@users.noreply.github.com>
2022-09-30 08:32:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+#Write your code below this line 👇
+
+print("Welcome to our 90 years time-left calculator")
+age= int(input("How old are you? "))
+time_left_till_90= 90 - age
+months_left= time_left_till_90 * 12
+weeks_left= months_left *4
+days_left= weeks_left * 30
+
+print (f"You have {time_left_till_90} years, {months_left} months, {weeks_left} weeks and {days_left} days left till you turn 90")
+
+
+
+
+
+
+
+
